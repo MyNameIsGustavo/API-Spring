15f400470a9d94206685f4e2b3a407c5ee844a78
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2024-09-15 11:42:55 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/apispring/apiSpring/entities/User.java
+++ src/main/java/com/apispring/apiSpring/entities/User.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -18,6 +20,9 @@ public class User implements Serializable {
     private String name;
     private String email;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -58,6 +63,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
